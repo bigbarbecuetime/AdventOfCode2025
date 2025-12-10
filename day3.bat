a113285991d36a96900efd57a30061ec3ae00bbc
bigbarbecuetime <nesworkprof@gmail.com>
2025-12-10 17:11:21 -0500

    Progress on day 3

--- day3.bat
+++ day3.bat
@@ -14,8 +14,8 @@ REM begin the while loop, while we have data to read
 
     REM read in the line
     SET /P line=
-
-    REM if the line has the EOF (CTRL+Z), goto end
+    REM EOF is detected by ERRORLEVEL being set from SET /P, stop
+    IF /I "%ERRORLEVEL%" NEQ "0" (GOTO END)
 
     REM find max for line 
     call:MaxForLine_ir %line%
@@ -25,19 +25,30 @@ REM begin the while loop, while we have data to read
 
 GOTO WHILE
 
-ECHO %total%
-GOTO END
-
 REM begin functions
 
 REM Inputs: String version of the line
 REM Outputs: Max Joltage for said line, on ir return variable
 :MaxForLine_ir
-ECHO TODO: Complete Max Function
-ECHO %~1
-SET /A ir=100
+SET /A max=0
+SET full_line=%~1
+
+REM TODO Add for loops
+REM For each char (i) (up to n-1)
+    SET chr1=%full_line:~0,1%
+    REM For each char past that i+1
+        SET chr2=%full_line:~1,1%
+        REM Combine the two into a guess
+        SET /A guess=%chr1%%chr2%
+        REM if the guess is greater than the maximum save it for that line
+        IF /I %guess% GTR %max% (SET /A max=%guess%)
+
+REM set the return to the max
+ENDLOCAL
+SET /A ir=%max%
+EXIT /B
 
 
 :END
-ENDLOCAL
+ECHO %total%
 EXIT /B 0
